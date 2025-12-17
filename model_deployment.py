a74c222308bf7cffbe38a534dbd19e70d04adef4
notnsas <ramapanjinararendra@gmail.com>
2025-12-17 13:28:57 +0700

    Modifikasi model deployment nambahin port

--- model_deployment.py
+++ model_deployment.py
@@ -3,9 +3,12 @@ import sqlalchemy.orm as so
 from app import app, db
 from app.models import User, Post
 
+
 @app.shell_context_processor
 def make_shell_context():
-    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
+    return {"sa": sa, "so": so, "db": db, "User": User, "Post": Post}
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
